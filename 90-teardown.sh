227abab44365595a1eb546d074f5f5267149992f
Ludwig Prager <ludwig.prager@celp.de>
2024-07-04 14:09:10 +0200

    ignores errors in teardown script

--- 90-teardown.sh
+++ 90-teardown.sh
@@ -5,9 +5,11 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/set-env.sh
 
+set +e
+
 echo deleting RG $RESOURCE_GROUP
 azcli az group delete --name ${RESOURCE_GROUP} --yes
 
 echo renaming kubeconfig
 MY_RANDOM=$RANDOM
-mv kubeconfig kubeconfig.${MY_RANDOM} 2> /dev/null || true
+mv kubeconfig kubeconfig.${MY_RANDOM} 2> /dev/null
